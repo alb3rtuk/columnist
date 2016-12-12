d3a76822bb31bedb0c6dc97d8eb73831c92f68c2
Albert Rannetsperger <alb3rtuk@hotmail.com>
2016-12-12 15:22:49 +0000

    Finally fixed the incorrect calculation of with when using encoded colors within the terminal output.

--- lib/columnist/column.rb
+++ lib/columnist/column.rb
@@ -5,10 +5,11 @@ module Columnist
         include OptionsValidator
 
         VALID_OPTIONS = [:width, :padding, :align, :color, :bold, :underline, :reversed]
-        attr_accessor :text, :size, *VALID_OPTIONS
+        attr_accessor :text, :text_without_colors, :size, *VALID_OPTIONS
 
         def initialize(text = nil, options = {})
             self.validate_options(options, *VALID_OPTIONS)
+            self.text_without_colors = text.to_s.gsub(/(.)\[\d{1,3};\d{1,3};\d{1,3}m/, '')
             self.text = text.to_s
             self.width = options[:width] || 10
             self.align = options[:align] || 'left'
@@ -17,7 +18,6 @@ module Columnist
             self.bold = options[:bold] || false
             self.underline = options[:underline] || false
             self.reversed = options[:reversed] || false
-
             raise ArgumentError unless self.width > 0
             raise ArgumentError unless self.padding.to_s.match(/^\d+$/)
         end
@@ -27,14 +27,37 @@ module Columnist
         end
 
         def required_width
-            self.text.to_s.size + 2 * self.padding
+            self.text_without_colors.to_s.size + 2 * self.padding
         end
 
         def screen_rows
-            if self.text.nil? || self.text.empty?
+            if self.text_without_colors.nil? || self.text_without_colors.empty?
                 [' ' * self.width]
             else
-                self.text.scan(/.{1,#{self.size}}/m).map { |s| to_cell(s) }
+                x = self.text.scan(/.{1,#{self.size}}/).map { |s| to_cell(s) }
+                if x.length > 1
+                    actual_length = 0
+                    x.each do |z|
+                        z = z.gsub(/\A(.)\[\d{1,3};\d{1,3};\d{1,3}m/, '').gsub(/\s+(.)\[\d{1,3};\d{1,3};\d{1,3}m\z/, '')
+                        characters_to_ignore = 0
+                        matches_to_ignore = z.scan(/\[\d{1,3};\d{1,3};\d{1,3}m/)
+                        matches_to_ignore.each do |match|
+                            characters_to_ignore += match.length + 1
+                        end
+                        actual_length += z.length - characters_to_ignore
+                    end
+                    if actual_length < self.size
+                        y = []
+                        x.each do |z|
+                            y << z.gsub(/\A(.)\[\d{1,3};\d{1,3};\d{1,3}m/, '').gsub(/\s+(.)\[\d{1,3};\d{1,3};\d{1,3}m\z/, '')
+                        end
+                        y = ["#{y.join}#{' ' * (self.size - actual_length)}"]
+                        return y
+                    end
+                    return [x[0]]
+                else
+                    return x
+                end
             end
         end
 
